1abd756e86e86482f409f34df5fa475d90475511
ALM-02 <150289145+ALM-02@users.noreply.github.com>
2023-12-12 10:14:16 +0000

    Add files via upload

---
+++ Adrian_Lorenzo_Mederos_RecuperacionTarea-main-master/frontend/src/App.js
@@ -0,0 +1,46 @@
+//Adrian Lorenzo Mederos
+import React from 'react'
+import './App.css';
+import Login from './components/Login'
+import Home from './components/Home'
+import Informes from './components/Informes'
+import GesUser from './components/GesUser'
+import GesPrestamos from './components/GesPrestamos'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+//importamos la función createBrowserRouter y el componente RouterProvider
+//de la librería react-router-dom
+//HACERLO
+const router = createBrowserRouter([
+    {
+        path: '/',
+        children: [
+            {
+                index: true,
+                element: <Login/>
+            },
+            {
+                path: 'home',
+                element: <Home/>
+            },
+            {
+                path: 'informes',
+                element: <Informes/>
+            },
+            {
+                path: 'gesuser',
+                element: <GesUser/>
+            },
+            {
+                path: 'gesprestamo',
+                element: <GesPrestamos/>
+            }
+        ]
+    }
+])
+function App() {
+    return (
+        <RouterProvider router={router} />
+    );
+}
+export default App;
+
